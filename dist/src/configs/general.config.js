da41986c53716d36edb516bc0e093b1b52dee389
Abdul Aliyev <aaa7c4@gmail.com>
2023-02-25 21:30:11 -0500

    Untrack not-needed stuff

--- dist/src/configs/general.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// @ts-nocheck
-// const general = {
-//   listPerPage: 10,
-// };
-// module.exports = general;
--- dist/src/interfaces/careers.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EStatus = void 0;
-var EStatus;
-(function (EStatus) {
-    EStatus["fail"] = "fail";
-    EStatus["success"] = "success";
-})(EStatus = exports.EStatus || (exports.EStatus = {}));
